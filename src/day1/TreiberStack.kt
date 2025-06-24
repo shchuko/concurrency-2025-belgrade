0eea97a3a7075a5a7bbde2b65d8119685c423308
Vladislav.Yaroshchuk <vladislav.yaroshchuk@jetbrains.com>
2025-06-24 11:53:58 +0200

    [task 3] TreiberStack: done

--- src/day1/TreiberStack.kt
+++ src/day1/TreiberStack.kt
@@ -7,21 +7,23 @@ class TreiberStack<E> : Stack<E> {
     private val top = AtomicReference<Node<E>?>(null)
 
     override fun push(element: E) {
-        // TODO: Make me linearizable!
-        // TODO: Update `top` via Compare-and-Set,
-        // TODO: restarting the operation on CAS failure.
-        val curTop = top.get()
-        val newTop = Node(element, curTop)
-        top.set(newTop)
+        while (true) {
+            val curTop = top.get()
+            val newTop = Node(element, curTop)
+            if (top.compareAndSet(curTop, newTop)) {
+                return
+            }
+        }
     }
 
     override fun pop(): E? {
-        // TODO: Make me linearizable!
-        // TODO: Update `top` via Compare-and-Set,
-        // TODO: restarting the operation on CAS failure.
-        val curTop = top.get() ?: return null
-        top.set(curTop.next)
-        return curTop.element
+        while (true) {
+            val curTop = top.get() ?: return null
+            val newTop = curTop.next
+            if (top.compareAndSet(curTop, newTop)) {
+                return curTop.element
+            }
+        }
     }
 
     private class Node<E>(
