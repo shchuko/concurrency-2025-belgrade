44ec1d26ddb2095263c819dcf13127cdac0f9342
Vladislav.Yaroshchuk <vladislav.yaroshchuk@jetbrains.com>
2025-06-24 13:31:18 +0200

    [task 4] MSQueue: done

--- src/day1/MSQueue.kt
+++ src/day1/MSQueue.kt
@@ -13,11 +13,26 @@ class MSQueue<E> : Queue<E> {
     }
 
     override fun enqueue(element: E) {
-        TODO("implement me")
+        val node = Node(element)
+        while (true) {
+            val curTail = tail.get()
+            if (curTail.next.compareAndSet(null, node)) {
+                tail.compareAndSet(curTail, node)
+                return
+            } else {
+                tail.compareAndSet(curTail, curTail.next.get())
+            }
+        }
     }
 
     override fun dequeue(): E? {
-        TODO("implement me")
+        while (true) {
+            val curHead = head.get()
+            val nextNode = curHead?.next?.get() ?: return null
+            if (head.compareAndSet(curHead, nextNode)) {
+                return nextNode.element
+            }
+        }
     }
 
     // FOR TEST PURPOSE, DO NOT CHANGE IT.
