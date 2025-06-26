efc406b275e8c34e89a6ff24ae477762ea8f5cb0
Vladislav.Yaroshchuk <vladislav.yaroshchuk@jetbrains.com>
2025-06-26 17:56:52 +0200

    [day 3 task 5] AtomicArrayWithCAS2Simplified: done

--- src/day3/AtomicArrayWithCAS2Simplified.kt
+++ src/day3/AtomicArrayWithCAS2Simplified.kt
@@ -17,9 +17,9 @@ class AtomicArrayWithCAS2Simplified<E : Any>(size: Int, initialValue: E) {
         }
     }
 
-    fun get(index: Int): E {
-        // TODO: the cell can store CAS2Descriptor
-        return array[index] as E
+    fun get(index: Int): E = when (val cur = array[index]) {
+        is AtomicArrayWithCAS2Simplified<E>.CAS2Descriptor -> cur[index]
+        else -> cur as E
     }
 
     fun cas2(
@@ -27,10 +27,17 @@ class AtomicArrayWithCAS2Simplified<E : Any>(size: Int, initialValue: E) {
         index2: Int, expected2: E, update2: E
     ): Boolean {
         require(index1 != index2) { "The indices should be different" }
-        val descriptor = CAS2Descriptor(
-            index1 = index1, expected1 = expected1, update1 = update1,
-            index2 = index2, expected2 = expected2, update2 = update2
-        )
+        val descriptor = if (index1 < index2) {
+            CAS2Descriptor(
+                index1 = index1, expected1 = expected1, update1 = update1,
+                index2 = index2, expected2 = expected2, update2 = update2
+            )
+        } else {
+            CAS2Descriptor(
+                index1 = index2, expected1 = expected2, update1 = update2,
+                index2 = index1, expected2 = expected1, update2 = update1
+            )
+        }
         descriptor.apply()
         return descriptor.status.get() === SUCCESS
     }
@@ -46,8 +53,84 @@ class AtomicArrayWithCAS2Simplified<E : Any>(size: Int, initialValue: E) {
         val status = AtomicReference(UNDECIDED)
 
         fun apply() {
-            // TODO: Install the descriptor, update the status, and update the cells;
-            // TODO: create functions for each of these three phases.
+            while (true) {
+                when (val value1 = array[index1]) {
+                    is AtomicArrayWithCAS2Simplified<E>.CAS2Descriptor -> {
+                        value1.installIntoIndex2()
+                        value1.doLogicalAndPhysicalApply()
+                    }
+
+                    else -> {
+                        if (value1 != expected1) {
+                            status.set(FAILED)
+                            return
+                        }
+
+                        if (!array.compareAndSet(index1, expected1, this)) {
+                            continue
+                        }
+
+                        installIntoIndex2()
+                        doLogicalAndPhysicalApply()
+                        return
+                    }
+                }
+            }
+        }
+
+        private fun installIntoIndex2() {
+            while (true) {
+                when (val value2 = array[index2]) {
+                    is AtomicArrayWithCAS2Simplified<E>.CAS2Descriptor -> {
+                        // already installed
+                        if (value2 === this) {
+                            return
+                        }
+
+                        value2.installIntoIndex2()
+                        value2.doLogicalAndPhysicalApply()
+                    }
+
+                    else -> {
+                        if (value2 != expected2) {
+                            status.compareAndSet(UNDECIDED, FAILED)
+                            return
+                        }
+
+                        // install yourself and exit on success
+                        if (array.compareAndSet(index2, expected2, this)) {
+                            return
+                        }
+                    }
+                }
+            }
+        }
+
+        private fun doLogicalAndPhysicalApply() {
+            status.compareAndSet(UNDECIDED, SUCCESS)
+
+            val status = status.get()
+            check(status != UNDECIDED)
+
+            val desiredValue2 = if (status == SUCCESS) update2 else expected2
+            val desiredValue1 = if (status == SUCCESS) update1 else expected1
+
+            array.compareAndSet(index2, this, desiredValue2)
+            array.compareAndSet(index1, this, desiredValue1)
+        }
+
+
+        operator fun get(index: Int): E {
+            val shouldTakeExpected = when (status.get()) {
+                UNDECIDED, FAILED -> true
+                SUCCESS -> false
+            }
+
+            return when (index) {
+                index1 -> if (shouldTakeExpected) expected1 else update1
+                index2 -> if (shouldTakeExpected) expected2 else update2
+                else -> error("Index $index does not match any [$expected1, $expected2]")
+            }
         }
     }
 
